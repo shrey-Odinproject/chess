fb988f60b603149c2c9eb649ef6291d517c5e5ef
shrey-Odinproject <sonishreyf1@gmail.com>
2022-02-06 18:53:26 +0530

    remove #setup_board from chessboard's interface , make it private and invoke during creation  rather than explicitly, modify chessboard tests accordingly

--- spec/chess_board_spec.rb
+++ spec/chess_board_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../lib/chess_board'
-# require_relative '../lib/pawn'
 
 describe ChessBoard do
   subject(:cb) { described_class.new }
@@ -9,29 +8,15 @@ describe ChessBoard do
   describe '#square_occupied?' do
     context 'when square is occupied' do
       it 'return true' do
-        cb.grid[0][7] = "\u2654"
-        tst = cb.square_occupied?(0, 7)
+        tst = cb.square_occupied?(0, 7) # a rondom occupied square
         expect(tst).to be true
       end
     end
 
     context 'when square is unoccupied' do
       it 'return false' do
-        cb.grid[0][7] = "\u2654"
-        tst = cb.square_occupied?(0, 6)
+        tst = cb.square_occupied?(3, 6) # random middle board square
         expect(tst).to be false
-      end
-    end
-  end
-
-  describe '#setup_pawns' do
-    context 'places white and black pawns on 7th and 2nd ranks' do
-      it 'deploys correct colored pawns' do
-        cb.setup_pawns
-        tst1 = cb.grid[1].all? { |square| square.color == 'B' }
-        tst2 = cb.grid[6].all? { |square| square.color == 'W' }
-        expect(tst1).to be true
-        expect(tst2).to be true
       end
     end
   end
