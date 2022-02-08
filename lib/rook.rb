03e7cfec510f4a77181160d374304028f1b9ba18
shrey-Odinproject <sonishreyf1@gmail.com>
2022-02-08 10:47:43 +0530

    fix rook hopping over piece bug , shift horse's test to abstract piece class's test as test for individual piece makes less sense rn, delete horse spec

--- lib/rook.rb
+++ lib/rook.rb
@@ -9,18 +9,32 @@ class Rook < ChessPiece
     color == 'W' ? "\u265c" : "\u2656"
   end
 
+  private
+
   def possible_moves
     possible = []
     steps = *(1..7)
     steps.each do |step|
       possible.push([row + step, column]) if chess_board.on_board?(row + step, column)
-      possible.push([row - step, column]) if chess_board.on_board?(row - step, column)
-      possible.push([row, column + step]) if chess_board.on_board?(row, column + step)
-      possible.push([row, column - step]) if chess_board.on_board?(row, column - step)
+      break if chess_board.occupied_square?(row + step, column)
+      # loop breaks as there is something in front so no more neighbor in that direction
     end
-    possible
 
-    # if pawn in front of rook eg rook at 0,0 and pawn at 1,0
-    # rooks valid move become 2,0 --- 7,0 which is false cause pawn blocks its vision
+    steps.each do |step|
+      possible.push([row - step, column]) if chess_board.on_board?(row - step, column)
+      break if chess_board.occupied_square?(row - step, column)
+    end
+
+    steps.each do |step|
+      possible.push([row, column + step]) if chess_board.on_board?(row, column + step)
+      break if chess_board.occupied_square?(row, column + step)
+    end
+
+    steps.each do |step|
+      possible.push([row, column - step]) if chess_board.on_board?(row, column - step)
+      break if chess_board.occupied_square?(row, column - step)
+    end
+
+    possible
   end
 end
---
+++ spec/chess_piece_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+require_relative '../lib/chess_piece'
+
+describe ChessPiece do
+  let(:c_b) { double('chess_board', show: nil) }
+  subject(:chess_piece) { described_class.new('B', c_b, 0, 1) }
+
+  describe '#move' do
+    before do
+      allow(chess_piece).to receive(:valid_moves).and_return [[2, 3], [7, 0]]
+    end
+
+    context 'when moving to a valid square' do
+      it 'moves the piece on board and updates the position' do
+        to_row = 7
+        to_column = 0 # 7,0 is present in array returned by valid_moves
+        expect(c_b).to receive(:move_piece_on_board).with(chess_piece, to_row, to_column).and_return(chess_piece)
+        expect(c_b).to receive(:show)
+        chess_piece.move(to_row, to_column)
+      end
+    end
+
+    context 'when moving to an invalid square' do
+      it 'displays error msg' do
+        to_row = 2
+        to_column = 5 # 2,5 is not in array returned by valid_moves
+        expect(c_b).not_to receive(:move_piece_on_board).with(chess_piece, to_row, to_column)
+        expect(c_b).not_to receive(:show)
+        expect(chess_piece).to receive(:puts).with("#{subject.class} cant move there")
+        chess_piece.move(to_row, to_column)
+      end
+    end
+  end
+end
