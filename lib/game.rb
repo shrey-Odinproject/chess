1f4675644572b206a3a5b29afd746bf4cbe3c26e
shrey-Odinproject <sonishreyf1@gmail.com>
2022-02-16 21:46:11 +0530

    create method to check legality of a move using the previous in_check? method (uses deepcopying of chess_board which is a shady area), num of arguments seem high and both king, in_check? now take chess_board as argument so they can run on 'copies' of the board

--- lib/game.rb
+++ lib/game.rb
@@ -7,7 +7,7 @@ class ChessGame
   attr_reader :chess_board, :pl_b, :pl_w
 
   def initialize
-    @chess_board = ChessBoard.new('8/P2k3r/3r4/8/2K5/4B1R1/8/N7')
+    @chess_board = ChessBoard.new('1B6/7r/3n4/4k1q1/8/6R1/8/N3K3')
     @pl_w = Player.new('W', chess_board)
     @pl_b = Player.new('B', chess_board)
   end
@@ -16,23 +16,37 @@ class ChessGame
     piece = chess_board.square(from_row, from_column)
 
     return puts 'this square is empty' if piece == ' '
-    return puts " #{player} this is not ur piece" if piece.color != player.color
+    return puts 'this is not ur piece' if piece.color != player.color
 
-    player.move_piece(piece, to_row, to_column) unless in_check?(king(player.color))
+    if legal_move?(player, from_row, from_column, to_row, to_column)
+      player.move_piece(piece, to_row, to_column)
+      chess_board.show
+    else
+      puts 'illegal move'
+    end
   end
 
   private
 
-  def king(color)
+  def legal_move?(player, from_row, from_column, to_row, to_column)
+    # makes move we tryna make on a copy of current board and sees if king is safe or not after the move
+    copy_board = Marshal.load(Marshal.dump(chess_board)) # using a deepcopy seems to work for now(clone/dup fail despite change in obj id)
+    copy_player = Player.new(player.color, copy_board)
+    copy_piece = copy_board.square(from_row, from_column)
+
+    copy_player.move_piece(copy_piece, to_row, to_column) # move piece on copy and see king's status
+    in_check?(king(copy_player.color, copy_board), copy_board) == false
+  end
+
+  def king(color, chess_board)
     chess_board.all_squares.each do |sq|
       return sq if sq.instance_of?(King) && sq.color == color
     end
   end
 
-  def in_check?(king)
+  def in_check?(king, chess_board)
     chess_board.all_squares.each do |sq|
       if sq != ' ' && sq.color != king.color && sq.valid_moves.include?([king.row, king.column])
-        chess_board.show
         puts " CHECK !! #{king} is under attack by #{sq}"
         return true # returns for 1st piece it finds which checks king
       end
@@ -42,4 +56,5 @@ class ChessGame
 end
 
 cg = ChessGame.new
-cg.validate_move_piece(cg.pl_w, 1, 0, 0, 0)
+cg.validate_move_piece(cg.pl_b, 1, 7, 7, 7)
+cg.validate_move_piece(cg.pl_w, 7, 4, 6, 4)
--- lib/player.rb
+++ lib/player.rb
@@ -10,9 +10,6 @@ class Player
   end
 
   def move_piece(piece, to_row, to_column)
-    # checking weather coordinates are valid wont happen here, here we assume arguments given are valid that checking needs to be seprate
-
     piece.move(to_row, to_column)
-    chess_board.show
   end
 end
